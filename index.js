2b2d74fa079d4238001c1a095270553278f56bf9
ch2isk4kos <ch2isk4kos@gmail.com>
2019-11-01 23:08:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// How .reduce() works:
+
+// const products = [
+//   { name: 'Shampoo', price: 4.99 },
+//   { name: 'Donuts', price: 7.99 },
+//   { name: 'Cookies', price: 6.49 },
+//   { name: 'Bath Gel', price: 13.99 }
+// ];
+
+// Solution 1:
+//
+// function getTotalAmountForProducts(products) {
+//   let totalPrice = 0;
+//
+//   products.forEach(product => {
+//     totalPrice += product.price;
+//   });
+//
+//   return totalPrice;
+// }
+//
+// console.log(getTotalAmountForProducts(products));                   // 33.46
+
+
+// Solution 2: further abstraction
+
+// function couponCounter(totalAmount, location) {
+//   return totalAmount + location.amount;
+// }
+//
+// console.log(reduce(couponLocations, couponCounter, 0));                // 15
+
+// -------------------------------------------------------
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce( (total, amount) => total += amount);
+
+// function solution:
+// const totalBatteries = () => {
+//     return batteryBatches.reduce( (tot, amt) => tot += amt);
+// }
